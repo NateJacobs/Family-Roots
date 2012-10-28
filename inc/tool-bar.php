03eabe55e0329971560033211798cb3d74fb9484
Nate Jacobs <nate@natejacobs.org>
2012-10-28 16:41:19 -0700

    First commit

---
+++ inc/tool-bar.php
@@ -0,0 +1,32 @@
+<?php
+
+familyRootsToolbar::init();
+
+/** 
+*	Family Roots Settings
+*
+*	Uses the settings API to display the settings page.
+
+*
+*	@author		Nate Jacobs
+*	@date		10/28/12
+*	@since		0.1
+*/
+class familyRootsToolbar
+{
+	/** 
+ 	*	Initialize
+ 	*
+ 	*	Hook into WordPress and prepare all the methods as necessary.
+ 	*
+ 	*	@author		Nate Jacobs
+ 	*	@date		10/28/12
+ 	*	@since		0.1
+ 	*
+ 	*	@param		null
+ 	*/
+	public static function init()
+	{
+		
+	}
+}
