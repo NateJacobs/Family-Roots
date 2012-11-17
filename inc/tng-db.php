7600903ea46a3f339ddc645f41467c02deb5354d
Nate Jacobs <nate@natejacobs.org>
2012-11-17 02:12:15 -0500

    Add second if clause to loop; Add code documentation

--- inc/tng-db.php
+++ inc/tng-db.php
@@ -44,31 +44,38 @@ class familyRootsTNGDatabase
 	*/
 	protected function get_tng_db_values()
 	{
+		// get array of config.php and customconfig.php
 		$results = familyRootsUtilities::get_tng_config();
 		
+		// do you have stuff?
 		if( !empty( $results ) )
 		{
 			$db_values = array();
 			
+			// loop through each line and find all the values that start with $database_ or $users_table
 			foreach( $results as $line ) 
 			{
+				// is it the $database_ value?
 				if( substr( trim( $line ), 0, 10 ) == '$database_' )
 				{
+					// split them on the =
 					$key = substr( trim( strstr( $line, '=', TRUE ) ), 10 );
 					$value = explode( '=', $line );
+					// take the first half and make it the key and the second half the value
 					$db_values[$key] = rtrim( str_replace('"', "", $value[1] ), ";" );
 				}
-			}
-			
-			foreach( $results as $line )
-			{
+				
+				// is it the $users_table value?
 				if( substr( trim( $line ), 0, 12 ) == '$users_table' )
 				{
+					// split them on the =
 					$value = explode( '=', $line );		
+					// take the first half and make it the key and the second half the value
 					$users_table = rtrim( str_replace('"', "", $value[1] ), ";" );
 				}
 			}
-			
+						
+			// update the values in the options table
 			update_option( 'family-roots-tng-db', 
 				array( 
 					'host' 			=> trim( $db_values['host'] ), 
@@ -94,15 +101,19 @@ class familyRootsTNGDatabase
 	*/
 	protected function connect()
 	{
+		// get the tng db values from the wp options table
 		$settings = (array) get_option( 'family-roots-tng-db' );
 
 		try
 		{
+			// create a new wpdb object
 			$tng_db = new wpdb( $settings['username'], $settings['password'], $settings['name'], $settings['host'] );
+			// now return it
 			return $tng_db;
 		}
 		catch( Exception $e )
 		{
+			// any problems?
 			echo $e->getMessage();
 		}
 	}
