fe037e2a314b4ab77d70ca1cf4b814f5a90e135e
Nate Jacobs <nate@natejacobs.org>
2012-11-17 20:37:20 -0500

    Revert "Revert "Leave to settings for now""
    This reverts commit dcb2aedb02f161dd1036f21101db90802f8ea28a.

--- inc/tng-db.php
+++ inc/tng-db.php
@@ -40,16 +40,15 @@ class familyRootsTNGDatabase
 	*
 	*	@param	null	
 	*/
-	protected function connect()
+	private function connect()
 	{
 		// get the tng db values from the wp options table
 		$settings = (array) get_option( 'family-roots-tng-db' );
 
 		try
 		{
-			// create a new wpdb object
+			// create a new wpdb object and return it
 			$tng_db = new wpdb( $settings['username'], $settings['password'], $settings['name'], $settings['host'] );
-			// now return it
 			return $tng_db;
 		}
 		catch( Exception $e )
@@ -94,16 +93,48 @@ class familyRootsTNGDatabase
 	/** 
 	*	Check TNG User
 	*
-	*	Does the specified user exist in the TNG database yet?
+	*	Does the specified user exist in the TNG database?
 	*
 	*	@author		Nate Jacobs
 	*	@date		11/17/12
 	*	@since		0.1
 	*
-	*	@param	null	
+	*	@param	int	$user_id	
 	*/
-	public function check_user()
+	public function check_user( $user_id )
 	{
+		// get the family-roots-tng-db settings into an array
+		$settings = (array) get_option( 'family-roots-tng-db' );
 		
+		// get tng user id from usermeta table
+		$tng_user_id = get_user_meta( $user_id, 'tng_user_id' );
+
+		// is there a tng user id for this WordPress user?
+		if( empty( $tng_user_id ) )
+		{
+			// if not, return false 
+			return FALSE;
+		}
+		else
+		{	
+			// if so, connect to TNG database and select userID of WordPress user specified
+			$users_table = $settings['users_table'];
+			$tng_user_id = $tng_user_id[0];
+
+			$tng_db = self::connect();
+			$tng_user = $tng_db->get_row( "SELECT userID FROM $users_table WHERE userID = '$tng_user_id'" );
+
+			// is there a TNG user with that userID?
+			if( $tng_user == NULL )
+			{
+				// if not, delete the usermeta attached to the specificed user as the user does not exist anymore
+				delete_user_meta( $user_ID, 'tng_user_id' );
+			}
+			else
+			{
+				// if so, return the userID to do other things				
+				return $tng_user->userID;
+			}	
+		}
 	}
 }
