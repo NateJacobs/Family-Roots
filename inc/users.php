bdc36b3b845a677277a0b8389e36c58d83cb3026
Nate Jacobs <nate@natejacobs.org>
2012-11-20 23:57:11 -0500

    Add delete user method and reorder check user

--- inc/users.php
+++ inc/users.php
@@ -1,6 +1,6 @@
 <?php
 
-//familyRootsUserManagement::init();
+familyRootsUserManagement::init();
 
 /** 
 *	Family Roots User Management
@@ -26,7 +26,12 @@ class familyRootsUserManagement extends familyRootsTNGDatabase
 	*/
 	public static function init()
 	{
-		//add_action( 'delete_user', array( __CLASS__, 'delete_user' ) );
+		$settings = (array) get_option( 'family-roots-users-settings' );
+		$sync_users = isset( $settings['sync_users'] ) ? $settings['sync_users'] : '';
+		if( $sync_users == 'on' )
+		{
+			add_action( 'delete_user', array( __CLASS__, 'delete_user' ) );
+		}
 	}
 	
 	/** 
@@ -44,23 +49,7 @@ class familyRootsUserManagement extends familyRootsTNGDatabase
 	{
 		
 	}
-	
-	/** 
-	*	Delete TNG User
-	*
-	*	Delete an existing user in the TNG database
-	*
-	*	@author		Nate Jacobs
-	*	@date		11/17/12
-	*	@since		0.1
-	*
-	*	@param	null	
-	*/
-	public function delete_user()
-	{
 		
-	}
-	
 	/** 
 	*	Update TNG User
 	*
@@ -78,9 +67,50 @@ class familyRootsUserManagement extends familyRootsTNGDatabase
 	}
 	
 	/** 
+	*	Delete TNG User
+	*
+	*	Delete an existing user in the TNG database
+	*
+	*	@author		Nate Jacobs
+	*	@date		11/17/12
+	*	@since		0.1
+	*
+	*	@param	int	$user_id	
+	*/
+	public function delete_user( $user_id )
+	{
+		$is_user = self::check_user( $user_id );
+		$error = '';
+		
+		if( $is_user )
+		{
+			// get the family-roots-tng-db settings into an array
+			$settings = (array) get_option( 'family-roots-tng-db' );
+			
+			// connect to TNG database and delete user specified
+			$users_table = $settings['users_table'];
+			$tngdb = parent::connect();
+			
+			$tng_user = $tngdb->query( 
+				$tngdb->prepare( "DELETE FROM $users_table WHERE userID = %d", $is_user ) 
+			);
+			
+			if( $tng_user != 1 )
+			{
+				$error = new WP_Error( 'tng_delete_user', __( "The user was not deleted", 'family-roots-integration' ) );
+			}
+			
+			if( is_wp_error( $error ) )
+			{
+				echo '<div id="message" class="error"><p>' . $error->get_error_message() . '</p></div>';
+			}
+		}
+	}
+	
+	/** 
 	*	Check TNG User
 	*
-	*	Does the specified user exist in the TNG database?
+	*	Check if the specified WordPress user exists in the TNG database.
 	*
 	*	@author		Nate Jacobs
 	*	@date		11/17/12
@@ -108,14 +138,16 @@ class familyRootsUserManagement extends familyRootsTNGDatabase
 			$users_table = $settings['users_table'];
 			$tng_user_id = $tng_user_id[0];
 
-			$tng_db = parent::connect();
-			$tng_user = $tng_db->get_row( "SELECT userID FROM $users_table WHERE userID = '$tng_user_id'" );
+			$tngdb = parent::connect();
+			$tng_user = $tngdb->get_row( "SELECT userID FROM $users_table WHERE userID = '$tng_user_id'" );
 
 			// is there a TNG user with that userID?
 			if( $tng_user == NULL )
 			{
 				// if not, delete the usermeta attached to the specificed user as the user does not exist anymore
-				delete_user_meta( $user_ID, 'tng_user_id' );
+				delete_user_meta( $user_id, 'tng_user_id' );
+				// return false
+				return false;
 			}
 			else
 			{
