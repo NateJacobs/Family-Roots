81b0aae37508b1c4ac1b517e8b10900a4f9547d9
Nate Jacobs <nate@natejacobs.org>
2014-08-24 23:14:11 -0700

    Standardize the person and family query.

--- family/class-family-query.php
+++ family/class-family-query.php
@@ -9,8 +9,7 @@
  */
 class TNG_Family_Query extends FamilyRootsTNGDatabase {
 	
-	public $db;
-	public $settings;
+	public $data;
 	
 	/** 
 	 *	Connect to the TNG database when the class is instantiated.
@@ -21,12 +20,20 @@ class TNG_Family_Query extends FamilyRootsTNGDatabase {
 	 */
 	public function __construct($query = null) {
 		if(!is_null($query)) {
-			$this->db = parent::connect();
-			$this->settings = get_option('family-roots-settings');
-			
 			$this->prepare_query($query);
 			$this->query();
 		}
+	}
+	
+	/** 
+	 *	Get the database connection and family-roots settings.
+	 *
+	 *	@author		Nate Jacobs
+	 *	@date		8/17/14
+	 *	@since		1.0
+	 */
+	private function get_db_settings() {
+		return ['tables' => get_option('family-roots-settings'), 'db' => parent::connect()];
 	}
 	
 	/** 
@@ -53,33 +60,5 @@ class TNG_Family_Query extends FamilyRootsTNGDatabase {
 	 */
 	public function query() {
 		
-	}
-	
-	/** 
-	 *	Return all the members of a family.
-	 *
-	 *	@author		Nate Jacobs
-	 *	@date		8/10/14
-	 *	@since		1.0
-	 *
-	 *	@param		string	$id	The family ID being requested
-	 */
-	public function get_family_by_id($id = null) {
-		
-		if(is_null($id)) {
-			return WP_Error('family_id_missing',__('The family ID is missing.','family-roots-integration'));
-		}
-		
-		$family_table = isset($this->settings['family_table']) ? $this->settings['family_table'] : null;
-		
-		if(is_null($family_table)) {
-			return WP_Error('family_table_missing',__('The family table name is missing. Make sure you have set the TNG path in the settings page.','family-roots-integration'));
-		}
-		
-		$family = $this->db->get_row( 
-			$this->db->prepare("SELECT * FROM {$family_table} WHERE familyID = %s", $id)
-		);
-		
-		return $family;
 	}
 }
--- person/class-person-query.php
+++ person/class-person-query.php
@@ -20,7 +20,18 @@ class TNG_Person_Query extends FamilyRootsTNGDatabase {
 	public function __construct() {
 		
 	}
-
+	
+	/** 
+	 *	Get the database connection and family-roots settings.
+	 *
+	 *	@author		Nate Jacobs
+	 *	@date		8/17/14
+	 *	@since		1.0
+	 */
+	private function get_db_settings() {
+		return ['tables' => get_option('family-roots-settings'), 'db' => parent::connect()];
+	}
+	
 	/** 
 	 *	
 	 *
