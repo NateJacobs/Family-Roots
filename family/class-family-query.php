d02aa270abe845d82e75103a3d67c41a9c088fd5
Nate Jacobs <nate@natejacobs.org>
2014-09-05 22:42:19 -0700

    Set up beginnings of tng family query.

--- family/class-family-query.php
+++ family/class-family-query.php
@@ -9,7 +9,21 @@
  */
 class TNG_Family_Query extends FamilyRootsTNGDatabase {
 	
-	public $data;
+	public $query_vars;
+	
+	public $query_fields;
+	
+	public $query_where;
+	
+	public $query_orderby;
+	
+	public $query_limit;
+	
+	private $settings;
+	
+	private $total_family = 0;
+	
+	private $results;
 	
 	/** 
 	 *	Connect to the TNG database when the class is instantiated.
@@ -20,30 +34,20 @@ class TNG_Family_Query extends FamilyRootsTNGDatabase {
 	 */
 	public function __construct($query = null) {
 		if(!is_null($query)) {
+			$this->settings = ['tables' => get_option('family-roots-settings'), 'db' => parent::connect()];
 			$this->prepare_query($query);
 			$this->query();
 		}
 	}
 	
 	/** 
-	 *	Get the database connection and family-roots settings.
-	 *
-	 *	@author		Nate Jacobs
-	 *	@date		8/17/14
-	 *	@since		1.0
-	 */
-	private function get_db_settings() {
-		return ['tables' => get_option('family-roots-settings'), 'db' => parent::connect()];
-	}
-	
-	/** 
-	 *	
+	 *	Prepare the query.
 	 *
 	 *	@author		Nate Jacobs
 	 *	@date		8/10/14
 	 *	@since		1.0
 	 *
-	 *	@param		
+	 *	@param		array	$query	The requested variables.
 	 */
 	public function prepare_query() {
 		
