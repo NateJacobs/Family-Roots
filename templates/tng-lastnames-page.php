8e91c4a871f3cb8aac3db3e0599830250a57e8d1
Nate Jacobs <nate@natejacobs.org>
2014-10-13 23:31:01 -0700

    Make the templates more generic to fit within the default themes.

--- templates/tng-lastnames-page.php
+++ templates/tng-lastnames-page.php
@@ -1,14 +1,22 @@
+<?php get_header(); ?>
 <?php $utilities = new FamilyRootsUtilities() ?>
 <?php $settings = get_option('family-roots-settings'); ?>
-<div class="page-header">
-	<h1>Last names in the <?php echo ucwords(strtolower($settings['default_tree'])); ?> family tree</h1>
+<div id="main-content" class="main-content">
+	<div class="page-content">
+		<div class="page-header">
+			<h1>Last names in the <?php echo ucwords(strtolower($settings['default_tree'])); ?> family tree</h1>
+		</div>
+		<p class="lead">Top 15 last names in the tree</p>
+		<?php echo family_roots_get_lastname_cloud(); ?>
+		<hr>
+		<?php $last_names = family_roots_unique_last_names(); ?>
+		<ul class="list-inline">
+			<?php foreach($last_names as $name): ?>
+				<li><a href="<?php echo trailingslashit(home_url('genealogy/lastname/').rawurlencode($name->lastname)); ?>"><?php echo $name->lastname; ?></a></li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
 </div>
-<p class="lead">Top 15 last names in the tree</p>
-<?php echo family_roots_get_lastname_cloud(); ?>
-<hr>
-<?php $last_names = family_roots_unique_last_names(); ?>
-<ul class="list-inline">
-	<?php foreach($last_names as $name): ?>
-		<li><a href="<?php echo trailingslashit(home_url('genealogy/lastname/').rawurlencode($name->lastname)); ?>"><?php echo $name->lastname; ?></a></li>
-	<?php endforeach; ?>
-</ul>
+<?php
+get_sidebar();
+get_footer();
--- templates/tng-places-page.php
+++ templates/tng-places-page.php
@@ -1,10 +1,18 @@
+<?php get_header(); ?>
 <?php $utilities = new FamilyRootsUtilities(); ?>
 <?php $places = $utilities->get_all_places(); ?>
-<h1>Places</h1>
-<?php if($places): ?>
-	<ul class="list-inline">
-	<?php foreach($places as $place): ?>
-		<li><a href="<?php echo $utilities->get_place_url($place->ID); ?>"><?php echo $place->place; ?></a></li>
-	<?php endforeach; ?>
-	</ul>
-<?php endif; ?>
+<div id="main-content" class="main-content">
+	<div class="page-content">
+		<h1>Places</h1>
+		<?php if($places): ?>
+			<ul class="list-inline">
+			<?php foreach($places as $place): ?>
+				<li><a href="<?php echo $utilities->get_place_url($place->ID); ?>"><?php echo $place->place; ?></a></li>
+			<?php endforeach; ?>
+			</ul>
+		<?php endif; ?>
+	</div>
+</div>
+<?php
+get_sidebar();
+get_footer();
